578f4ce3e7d95a3637de6ad569ee5ab338ad4808
Direwolf314 <adkinsf@ccs.neu.edu>
2013-10-06 18:06:02 -0400

    Look in source for comments on how to use

---
+++ examplesToTests.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+import sys, re
+
+'''
+How to use me:
+
+Usage: ./thisfile Filename.rkt
+
+Parses through all comments in racket code and looks for sections that begin with ";;Examples:" and end with ";;Design Strategy:"
+It then takes those lines, looks for an equal sign, and throws both sides as new lines at the end of the file. 
+It also takes any other comments in those sections and translates them down to the bottom as well.
+There may be some extra new lines but at least you don't have to copy and paste by hand.
+
+Here's an example case:
+
+MyFile.rkt -
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;FUNCTIONS
+
+;;double : Number -> Number
+;;GIVEN: a number
+;;RETURN: that same number, doubled
+;;Examples:
+;;My comment here-
+;; (double 2) = 4
+;;My next comment-
+;; (double 8) = 16
+;;My final comment-
+;; (double 42) = 84
+(define (double x)
+    (* x 2))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;TESTS
+
+[originally empty, but after running ./thisfile MyFile.rkt it adds the following]
+
+;;My comment here-
+(double 2)
+4
+
+;;My next comment-
+(double 8)
+16
+
+;;My final comment-
+(double 42)
+84
+
+You can then proceed to copy+paste your heart out with "(check-equals?" and "This failed because you suck")
+
+I would have done more to automatically add those parts but the logic was getting unwieldly. 
+
+Enjoy!
+-d1r3w0lf
+
+'''
+
+#Opens the given filename and converts all examples (in comments) 
+def main(filename):
+	f = open(filename, "r")
+	contents = f.read().split(";;Examples:")
+	f.close()
+
+	f = open(filename, "a")
+	for i in xrange(len(contents)):
+		if i == 0:
+			continue
+		group = contents[i].split(";;Design Strategy:")[0]
+		lines = group.split("\n")
+		# print group
+		for x in lines:
+			if x[0:3] != ";; " and len(x) > 1: # a comment - throw it at the end
+				f.write(x + "\n")
+				# print "comment is: " + "'" + x + "'"
+			else:
+				for part in x.split("="):
+					f.write(part.replace(";;","") + "\n")
+
+
+	f.close()
+
+
+def usage(myName):
+	print "Usage:\n\t%s Filename.rkt" % myName
+
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		usage(sys.argv[0])
+	else:
+		main(sys.argv[1])
---
+++ nbGen.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+import os, datetime
+
+filename = "notebook.txt"
+date = datetime.datetime.now().strftime('%m/%d')
+start = datetime.datetime.now().strftime('%H:%M')
+stop = datetime.datetime.now().strftime('%H:%M')
+who = "FA"
+interruptions = "0"
+question = "1"
+comments = " "
+justChanged = False
+
+def getInterruptions():
+	if interruptions[0] == " ":
+		return 0
+	else:
+		return eval(interruptions)
+
+def getTimeOnTask():
+	return (int(stop.split(":")[0]) - int(start.split(":")[0]))*60 + (int(stop.split(":")[1]) - int(start.split(":")[1])) - getInterruptions()
+
+def printEntry():
+	print "Date\tWho\tStart\tStop\tInterruptions\tQuestion\tTimeOnTask\tComments"
+	print "%(date)s\t%(who)s\t%(start)s\t%(stop)s\t%(interruptions)s\t\t%(question)s\t\t%(timeOnTask)d\t\t%(comments)s" % \
+		{"timeOnTask" : getTimeOnTask(), "date" : date, "who" : who, "start" : start, "stop" : stop, 
+		 "interruptions": interruptions, 
+		 "question" : question, 
+		 "comments" : comments}
+
+def printFile():
+	print "="*30
+	print "Contents of " + filename + " - "
+	print open(filename,'r').read()
+	print "="*30
+
+def putFirstLine():	
+	f = open(filename, 'w')
+	f.write("Date\tWho\tStart\tStop\tInterruptions\tQuestion\tTimeOnTask\tComments\n")
+	f.close()
+
+def putEntry():
+	f = open(filename, 'a')
+	f.write("%(date)s\t%(who)s\t%(start)s\t%(stop)s\t%(interruptions)s\t\t\t\t%(question)s\t\t\t%(timeOnTask)d\t\t\t%(comments)s\n" % \
+		{"timeOnTask" : getTimeOnTask(), "date" : date, "who" : who, "start" : start, "stop" : stop, 
+		 "interruptions": interruptions, 
+		 "question" : question, 
+		 "comments" : comments})
+	f.close()
+
+	global start, stop
+	start = datetime.datetime.now().strftime('%H:%M')
+	stop = datetime.datetime.now().strftime('%H:%M')
+	
+
+def putGitEntry():
+	f = open(filename,'a')
+	f.write("============committing to git: %(date)s %(time)s ===================\n" % {"date" : date, "time" : datetime.datetime.now().strftime('%H:%M')})
+	f.close()
+
+def getTotals():
+	contents = open(filename,'r').readlines()[1:] #Skip first line
+	totals = {}
+	for i in contents:
+		if i[0:3] == "===" or len(i) < 5: #arbitrary 5 means not just a \n
+			continue # skip git statements and empty lines
+		theSplit = i.split()
+		if theSplit[5] in totals:
+			totals[theSplit[5]] = totals[theSplit[5]] + int(theSplit[6])
+		else: 
+			totals[theSplit[5]] = int(theSplit[6])
+	return totals
+
+def putTotals():
+	totals = getTotals()
+	f = open(filename, 'a')
+	f.write("\n")
+	for i in sorted(totals.iterkeys()):
+		m = "%d" % totals[i]
+		label = "Total Time On Task Q" + str(i) + " (minutes)"
+		f.write(label + " "*(52-len(m)-len(label)) + m +"\n")
+	for i in sorted(totals.iterkeys()):
+		hAndT = "%.1f" % (float(totals[i])/60)
+		label = "TOTQ" + str(i) + " (hours and tenths)"
+		f.write(label + " "*(52-len(hAndT)-len(label)) + hAndT + "\n")
+	f.close()
+
+def clearScreen():
+	os.system("clear")
+
+def vim():
+	os.system("vim " + filename)
+
+if __name__ == "__main__":
+	clearScreen()
+	while(True):
+		if not(justChanged):
+			stop = datetime.datetime.now().strftime('%H:%M')
+		justChanged = False
+		print "The current entry is:"
+		printEntry()
+		command = raw_input("Please enter a command:")
+		theSplit = command.split()
+
+		comm = ""
+		arg = ""
+
+		if len(theSplit) > 1:
+			comm = theSplit[0]
+			arg = command[(len(comm)+1):]
+		else:
+			comm = theSplit[0]
+
+		try:
+
+			if comm == "h" or comm == "help":
+				print "Commands are:"
+				print "\t(h)elp"
+				print "\t(d)ate [date] - defaults to today"
+				print "\t(w)ho [initials] - defaults to 'who' global"
+				print "\tstart [time] - defaults to current time"
+				print "\tstop [time] - defaults to current time"
+				print "\t(i)nterruptions [num] - appends to current list with a '+', no args will clear"
+				print "\t(q)uestion <question>"
+				print "\t(c)omment <comment>"
+				print "\t(pfl) - put first line (Date, Who, etc)"
+				print "\t(p)ut - puts the entry in the file"
+				print "\t(g)it - puts a git push entry in the file"
+				print "\t(t)otal - adds totals to the end of the file"
+				print "\t(f)ile <filename> - change the filename (and path)"
+				print "\t(cls) - clear screen"
+				print "\t(vim) - open the notebook in vim"
+				print "\t(pf) - print the full file"
+				print "\tquit/(e)xit"
+				continue
+			elif comm == "d" or comm == "date":
+				date = arg
+			elif comm == "w" or comm == "who":
+				who = arg
+			elif comm == "start":
+				if len(arg) == 0:
+					start = datetime.datetime.now().strftime('%H:%M')
+				else:
+					start = arg
+			elif comm == "stop":
+				if len(arg) == 0:
+					stop = datetime.datetime.now().strftime('%H:%M')
+				else:
+					stop = arg
+					justChanged = True
+			elif comm == "i" or comm == "interruptions":
+				if len(arg) == 0:
+					interruptions = "0"
+				elif interruptions[0] == "0":
+					interruptions = arg
+				else:
+					interruptions = interruptions + "+" + arg
+			elif comm == "q" or comm == "question":
+				question = arg
+			elif comm == "c" or comm == "comment":
+				comments = arg
+			elif comm == "pfl":
+				putFirstLine()
+			elif comm == "p" or comm == "put":
+				putEntry()
+			elif comm == "g" or comm == "git":
+				putGitEntry()
+			elif comm == "t" or comm == "total":
+				putTotals()
+			elif comm == "f" or comm == "file":
+				filename = arg
+			elif comm == "cls":
+				clearScreen()
+			elif comm == "vim":
+				vim()
+			elif comm == "pf":
+				try:
+					printFile()
+				except IOError:
+					print "File doesn't exist - use pfl to make it"
+				continue
+			elif comm == "quit" or comm == "e" or comm == "exit":
+				break
+			else:
+				print "Invalid command"
+				continue
+			clearScreen()
+		except:
+			print "Invalid command\n"
