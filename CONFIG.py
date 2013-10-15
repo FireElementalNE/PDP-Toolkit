20237e9b778b97b618f0c1322765311bb52df32f
FireElementalNE <FireElemental.NE@Gmail.com>
2013-10-15 13:26:27 -0400

    added whostring call for easy changes

--- CONFIG.py
+++ CONFIG.py
@@ -3,3 +3,4 @@ CONGRATSLINEMESSEGE = 'All lines were under 80 characters! Congratz!'
 CONGRATSLAMBDAMESSEGE = 'No lambda functions were incorrect! CONGRAZ!'
 STARTAT = -2
 PROBLEMSET = 4
+WHOSTRING = 'LK'
