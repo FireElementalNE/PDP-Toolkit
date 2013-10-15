a296f1267181e1584258095c429e5fffb0892f72
FireElementalNE <FireElemental.NE@Gmail.com>
2013-10-15 10:37:24 -0400

    added lambda function analysis

--- CONFIG.py
+++ CONFIG.py
@@ -1,2 +1,3 @@
 NUMEQUALS = 21
 CONGRATSLINEMESSEGE = "All lines were under 80 characters! Congratz!"
+STARTAT = 1
--- reportFunctions.py
+++ reportFunctions.py
@@ -1,4 +1,5 @@
 import re,datetime,os,platform
+import collections
 from CONFIG import *
 
 myOS = platform.system()
@@ -19,6 +20,7 @@ def runReports(fullFileName, fileName, directory):
 			content = f.readlines()
 	lineReport = runLineReport(content)
 	commentReport = commentCodeRatio(content)
+	lambdaReport = lambdaAnalysis(content)
 	theTime = datetime.datetime.now().strftime("%H:%M %B %d, %Y")
 	outFh.write('Report on ' + fileName + '\n')
 	outFh.write('Run on ' + theTime + '\n\n')
@@ -27,8 +29,11 @@ def runReports(fullFileName, fileName, directory):
 		outFh.write(CONGRATSLINEMESSEGE + '\n' + '\n')
 	else:
 		outFh.write(lineReport[0])
+	outFh.write('LAMBDA REPORT ' + '\n' + EQUALSSTRING)
+	outFh.write(lambdaReport)
 	outFh.write('COMMENT/CODE REPORT ' + '\n' + EQUALSSTRING)
 	outFh.write(commentReport)
+
 
 def isRacketFile(fileName):
 	racketRe = '^.*\.rkt$'
@@ -37,7 +42,7 @@ def isRacketFile(fileName):
 	return False
 
 def runLineReport(content):
-	lineNum = 1
+	lineNum = STARTAT
 	returnString = ''
 	count = 0
 	for x in content:
@@ -64,6 +69,48 @@ def commentCodeRatio(content):
 	commentLines = 'Number of Lines with Comments: ' + str(int(comments))
 	emptyLines = 'Number of Empty Lines:         ' + str(int(empty))
 	return codeLines + '\n' + commentLines + '\n' + emptyLines + '\n' + 'Total Lines: ' + str(len(content)) + '\n' + 'Code to total Ratio: ' + str(codeToWaste) + '\n'
+
+def lambdaAnalysis(content):
+	badDict = {}
+	lambdaRegex = '.*\(lambda.*\n'
+	lambdaRegex2 = '.*\;'
+	lineFixer = '^[\t\s]*(.*)\n$'
+	lambdaCount = 0
+	lambdaGood = 0
+	lambdaBad = 0
+	lineNum = STARTAT
+	myFlag = False
+	prevLine = None
+	for x in content:
+		if myFlag:
+			if re.match(lambdaRegex2,x):
+				lambdaGood = lambdaGood + 1
+				myFlag = False
+			else:
+				m = re.match(lineFixer,prevLine)
+				badDict[lineNum-1] = m.group(1)
+				lambdaBad = lambdaBad + 1
+				myFlag = False
+
+		if re.match(lambdaRegex,x):
+			lambdaCount = lambdaCount + 1
+			prevLine = x
+			myFlag = True
+		lineNum = lineNum + 1
+	total = 'Found ' + str(lambdaCount) + 'lambda Functions' + '\n'
+	good = None
+	if lambdaGood == 1:
+		good = str(lambdaGood) + ' is apparently all set' + '\n'
+	else:
+		good = str(lambdaGood) + ' are apparently all set' + '\n'
+	bad = str(lambdaBad) + ' of these have no contract and are as follows: ' + '\n'
+	badString = ''
+	badDict = collections.OrderedDict(sorted(badDict.items()))
+	#badDict = sorted(badDict, key=lambda key: badDict[key])
+	for key,value in badDict.iteritems():
+		badString = badString + '\t' + 'line ' + str(key) + ' had this: ' + value + '\n'
+	return total + good + bad + badString + '\n'
+
 
 
 
